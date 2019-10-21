630bd9e6e26ab44fd250495a1543f9aaf5627531
Jiyeon Seo <jiyeonseo@fb.com>
2019-10-21 14:37:34 +0900

    add title

--- src/components/ProductSummary/index.js
+++ src/components/ProductSummary/index.js
@@ -8,6 +8,7 @@ import AddToCart from '../AddToCart'
 export default ({ id, name, meta, sku, mainImage }) => (
   <Item.Group>
     <Helmet>
+
         <meta property="og:title" content={name} />
         <meta property="og:description" content={name} />
         <meta property="og:url" content={'https://agitated-pike-1d9669.netlify.com/product/'+id+'/'} />
@@ -18,6 +19,8 @@ export default ({ id, name, meta, sku, mainImage }) => (
         <meta property="product:price:amount" content='1000' />
         <meta property="product:price:currency" content='KRW' />
         <meta property="product:retailer_item_id" content={id} />
+        <title>Product_{name}</title>
+
     </Helmet>
     <Item style={{ alignItems: 'center' }}>
       <Item.Image size="medium">
